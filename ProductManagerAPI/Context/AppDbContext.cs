6b49ae20a82bea02f83c847fc84eca5d0ba84e74
Thiago Augusto <taugusto964@gmail.com>
2024-12-05 15:15:49 -0300

    feature: Tabelas do banco
    Criadas classe de contexto e tabelas do bd

---
+++ ProductManagerAPI/Context/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProductManagerAPI.Models;
+
+namespace ProductManagerAPI.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Produto> Produtos { get; set; }
+        public DbSet<Categoria> Categorias { get; set; }
+    }
+}
