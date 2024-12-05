943ff02091787dbc079231f71b1610cacef0f0aa
Thiago Augusto <taugusto964@gmail.com>
2024-12-05 13:50:40 -0300

    feature: adicionado models

---
+++ ProductManagerAPI/Models/Categoria.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace ProductManagerAPI.Models
+{
+    public class Categoria
+    {
+        public Categoria()
+        {
+            Produtos = new Collection<Produto>();
+        }
+
+        [Required]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Nome { get; set; }
+
+        public ICollection<Produto> Produtos { get; set; }
+    }
+}
---
+++ ProductManagerAPI/Models/Produto.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ProductManagerAPI.Models
+{
+    public class Produto
+    {
+        [Required]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Nome { get; set; }
+
+        [StringLength(200)]
+        public string Descricao {  get; set; }
+        public Status Status { get; set; }
+
+        [Required]
+        [Column(TypeName = "decimal(10,2)")]
+        public decimal Preco {  get; set; }
+
+        [Range(0, 9999)]
+        public int Quantidade {  get; set; }
+
+        public Categoria Categoria { get; set; }
+
+        public int CategoriaId {  get; set; }
+    }
+    public enum Status
+    {
+        Indisponivel = 0,
+        EmEstoque = 1
+    }
+}
