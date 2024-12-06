7bb54595ae29f557e3d10bc1933b369fba4878d4
Thiago Augusto <taugusto964@gmail.com>
2024-12-06 12:50:18 -0300

    Update Program.cs
    Serviços registrados

--- ProductManagerAPI/Program.cs
+++ ProductManagerAPI/Program.cs
@@ -1,5 +1,7 @@
 using Microsoft.EntityFrameworkCore;
 using ProductManagerAPI.Context;
+using ProductManagerAPI.Repositories;
+using ProductManagerAPI.Repositories.Interfaces;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,6 +13,10 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer("name=ConnectionStrings:DefaultConnection"));
+builder.Services.AddScoped<ICategoriaRepository,ICategoriaRepository>();
+builder.Services.AddScoped<IProdutoRepository,IProdutoRepository>();
+builder.Services.AddScoped(typeof(IBaseRepository<>), typeof(BaseRepository<>));
+builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 
 var app = builder.Build();
 
