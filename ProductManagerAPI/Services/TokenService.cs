b5dc6ff5bc1bd58a842d0c9ce6cfff9452640db6
Thiago Augusto <taugusto964@gmail.com>
2025-01-10 08:14:41 -0300

    feature: Adicionadas classes de DTOs e serviço para gerenciar token

---
+++ ProductManagerAPI/Services/TokenService.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace ProductManagerAPI.Services
+{
+    public class TokenService : ITokenService
+    {
+        public JwtSecurityToken GenerateAccessToken(IEnumerable<Claim> claims, IConfiguration _config)
+        {
+            //pega a chave secreta da seção JWT no arquivo appSettings.json
+            var key = _config.GetSection("JWT").GetValue<string>("SecretKey") ??
+               throw new InvalidOperationException("Invalid secret Key");
+
+            //converte a chave para um array de  bytes
+            var privateKey = Encoding.UTF8.GetBytes(key);
+
+            //cria as credenciais para assinatura usando a chave criptografada
+            var signingCredentials = new SigningCredentials(new SymmetricSecurityKey(privateKey),
+                                     SecurityAlgorithms.HmacSha256Signature);
+
+            //especifica as descriçoes do token
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                //claims do usuário
+                Subject = new ClaimsIdentity(claims),
+                Expires = DateTime.UtcNow.AddMinutes(_config.GetSection("JWT")
+                                                    .GetValue<double>("TokenValidityInMinutes")),
+                Audience = _config.GetSection("JWT")
+                                  .GetValue<string>("ValidAudience"),
+                Issuer = _config.GetSection("JWT").GetValue<string>("ValidIssuer"),
+                SigningCredentials = signingCredentials
+            };
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var token = tokenHandler.CreateJwtSecurityToken(tokenDescriptor);
+            return token;
+        }
+
+        public string GenerateRefreshToken()
+        {
+
+            var secureRandomBytes = new byte[128];
+
+            //gera numeros aleatórios
+            using var randomNumberGenerator = RandomNumberGenerator.Create();
+
+            //Preenche o array de bytes com os números gerados
+            randomNumberGenerator.GetBytes(secureRandomBytes);
+
+            //converte o array de bytes para uma representação em string em base 64 do conteúdo do array
+            var refreshToken = Convert.ToBase64String(secureRandomBytes);
+
+            return refreshToken;
+        }
+
+        public ClaimsPrincipal GetPrincipalFromExpiredToken(string token, IConfiguration _config)
+        {
+            var secretKey = _config["JWT:SecretKey"] ?? throw new InvalidOperationException("Invalid key");
+
+            var tokenValidationParameters = new TokenValidationParameters
+            {
+                ValidateAudience = false,
+                ValidateIssuer = false,
+                ValidateIssuerSigningKey = true,
+                IssuerSigningKey = new SymmetricSecurityKey(
+                                      Encoding.UTF8.GetBytes(secretKey)),
+                ValidateLifetime = false
+            };
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+
+            var principal = tokenHandler.ValidateToken(token, tokenValidationParameters,
+                                                       out SecurityToken securityToken);
+
+            if (securityToken is not JwtSecurityToken jwtSecurityToken ||
+                             !jwtSecurityToken.Header.Alg.Equals(
+                             SecurityAlgorithms.HmacSha256,
+                             StringComparison.InvariantCultureIgnoreCase))
+            {
+                throw new SecurityTokenException("Invalid token");
+            }
+            return principal;
+        }
+    }
+}
